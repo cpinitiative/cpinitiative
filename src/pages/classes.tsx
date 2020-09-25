087d91baabf995d75c316edf00833fc519750446
Nathan Wang <nathan.r.wang@gmail.com>
2020-09-24 17:47:49 -0700

    feat: add CTA to classes page

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -45,7 +45,7 @@ export default function Classes() {
         </div>
         <div className="relative px-4 sm:px-6 lg:px-8">
           <div className="text-lg max-w-prose mx-auto mb-6">
-            <p className="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Course Info</p>
+            <p className="text-base text-center leading-6 text-purple-600 font-semibold tracking-wide uppercase">Course Info</p>
             <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">Class Format</h1>
           </div>
           <div className="prose prose-lg text-gray-500 mx-auto">
@@ -117,6 +117,28 @@ export default function Classes() {
               <li>Euler Tour Technique</li>
             </ul>
 
+          </div>
+        </div>
+      </div>
+
+      <div className="bg-gray-50">
+        <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+            Ready to get started?
+            <br />
+            <span className="text-purple-600">Join CPI Classes today!</span>
+          </h2>
+          <div className="mt-8 flex lg:flex-shrink-0 lg:mt-0">
+            <div className="inline-flex rounded-md shadow">
+              <a href="https://forms.gle/ZnxAefowRcXoaCgx7" target="_blank" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                Apply Now
+              </a>
+            </div>
+            <div className="ml-3 inline-flex rounded-md shadow">
+              <a href="mailto:usacoguide@gmail.com" target="_blank" className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-purple-600 bg-white hover:text-purple-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                Contact Us
+              </a>
+            </div>
           </div>
         </div>
       </div>
