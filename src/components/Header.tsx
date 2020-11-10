71098b5815c116a577a9cfc3983ec680a0b91396
Nathan Wang <nathan.r.wang@gmail.com>
2020-11-10 15:54:38 -0800

    upd

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ export default function Header({noBanner}:{
   return (
 
     <nav className="bg-white shadow fixed inset-x-0 top-0 z-30">
-      {!noBanner && <div className="relative bg-indigo-600">
+      {!noBanner && <div className="relative bg-indigo-700">
         <div className="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
         <div className="pr-16 sm:text-center sm:px-16">
           <p className="font-medium text-white">
--- src/pages/webinar.tsx
+++ src/pages/webinar.tsx
@@ -19,6 +19,21 @@ export default function Webinar({data}) {
       <Hero />
       <AgendaAndContent />
       <WebinarPeople />
+
+      <div className="relative py-16 bg-white overflow-hidden">
+        <div className="relative px-4 sm:px-6 lg:px-8">
+          <div className="text-lg max-w-prose mx-auto mb-6">
+            <p className="text-base text-center leading-6 text-indigo-600 font-semibold tracking-wide uppercase">Our Story</p>
+            <h1 className="mt-2 mb-8 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">Why do USACO?</h1>
+            <p className="text-xl text-gray-500 leading-8">Participating in USACO is an excellent way for you to meet the best CS students across the country, build friendships, and kickstart your CS career.</p>
+          </div>
+          <div className="prose prose-lg text-gray-500 mx-auto">
+            <p>USACO alumni are prevalent in both academia and industry, and have gone on to create amazing companies such as Quora. Unfortunately, not many students are aware of or participate in USACO.</p>
+            <p>That's where we come in! The Competitive Programming Initiative is a nonprofit organization run by top USACO competitors that aims to increase awareness and participation in USACO. Our team members include past USACO Finalists and IOI participants.</p>
+            <p>We hope you'll join us to learn more about USACO and why it can be a good choice for you!</p>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
