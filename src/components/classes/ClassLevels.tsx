e410cbc8954ba94238b2b3c9acba48bde9fafcde
Nathan Wang <nathan.r.wang@gmail.com>
2020-11-24 18:54:59 -0800

    upd

--- src/components/classes/ClassLevels.tsx
+++ src/components/classes/ClassLevels.tsx
@@ -116,7 +116,7 @@ export default function ClassLevels() {
                 </div>
               </div>
               <div className="mt-6 rounded-md shadow lg:mt-0 lg:ml-10 lg:flex-shrink-0">
-                <a href="https://docs.google.com/forms/d/e/1FAIpQLScmeNhI6SK1RmVqs2BiTEXADADnkFIPwa_T7yC6C2YkrRbBZA/viewform" target="_blank" className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-900 bg-white hover:text-gray-700 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLScKCrke0SfKBwqCrk0VtfXZsgdv-QP2xehU4hQ-Slg-wLyXGA/viewform" target="_blank" className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-900 bg-white hover:text-gray-700 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
                   Apply Now
                 </a>
               </div>
