4ecb4787982697a7b1e33a2ef86d2faf09795889
Jeffrey Meng <jeffkmeng@gmail.com>
2021-03-14 21:20:32 -0700

    fix: fix financial aid button color

--- src/pages/classes/admin/view-registrations.tsx
+++ src/pages/classes/admin/view-registrations.tsx
@@ -491,7 +491,7 @@ export default function ViewRegistrationPage() {
                         })
                       }}
                       type="button"
-                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                      className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-purple-600 text-base font-medium text-white hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
                     >
                       Grant Full Financial Aid
                     </button>
