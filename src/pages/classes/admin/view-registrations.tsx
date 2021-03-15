d09d454a2a0fae4542b95eaf5b313f6bbcb48179
Nathan Wang <nathan.r.wang@gmail.com>
2021-03-14 22:10:36 -0700

    fix close modal scrolling

--- src/pages/classes/admin/view-registrations.tsx
+++ src/pages/classes/admin/view-registrations.tsx
@@ -519,7 +519,8 @@ export default function ViewRegistrationPage() {
                   <button
                     type="button"
                     onClick={() => {
-                      window.location.hash = "#"
+                      history.pushState(null, null, ' ');
+                      setShowDetailModal(false);
                     }}
                     className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500 sm:mt-0 sm:w-auto sm:text-sm"
                   >
