265d2d828e9e1bcae24e3bdffb84f33e918be8fa
Nathan Wang <nathan.r.wang@gmail.com>
2021-07-02 01:27:26 -0700

    minor

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -39,7 +39,7 @@ export default function Classes() {
                         Finalists. Designed from the ground-up to embrace the
                         self-study video format.
                       </p>
-                      <div className="mt-8 sm:mt-12 sm:space-x-4 space-y-4 sm:space-y-0">
+                      <div className="mt-8 sm:mt-12 space-x-4">
                         <a
                           href="https://docs.google.com/forms/d/e/1FAIpQLSc0IgtbVoYsWv_2E2ZIcN8UE29OVpmx6FsiM0x9lfwNdGey3Q/viewform"
                           className="inline-flex sm:text-lg py-3 px-6 sm:py-4 sm:px-8 rounded-md shadow bg-gradient-to-r from-teal-500 to-cyan-600 text-white font-medium hover:from-teal-600 hover:to-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-400 focus:ring-offset-gray-900"
