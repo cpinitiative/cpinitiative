e23916d10aa1b3e20074d413a9977789e2a837e5
cskitty <melody.yue.yu@gmail.com>
2021-07-22 18:58:31 -0700

    USACO workshop page

---
+++ src/components/workshop/AgendaAndContent.tsx
@@ -0,0 +1,30 @@
+import { Link } from "gatsby"
+import * as React from "react"
+
+export default function AgendaAndContent() {
+  return (
+    <div className="bg-purple-900 py-16 sm:py-20">
+      <div className="mx-auto max-w-4xl px-4 sm:px-6">
+        <h1 className="text-4xl font-black text-white text-center mb-8">
+          Agenda
+        </h1>
+        <div className="text-lg text-purple-200">
+          <p className="text-xl text-white mb-4 font-bold">
+            During the webinar, experienced USACO contestants will explain
+            everything you need to know about getting started with USACO.
+          </p>
+          <ul className={"list-disc ml-5"}>
+            <li>What is USACO</li>
+            <li>When and How to Attend USACO</li>
+            <li>USACO Level</li>
+            <li>Programming Languages for USACO</li>
+            <li>USACO Scoring</li>
+            <li>USACO Sample Question Workthrough?</li>
+            <li>What Should I Learn for USACO</li>
+            <li>Open Q&A</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/workshop.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import Hero from "../components/workshop/Hero"
+import Header from "../components/Header"
+import AgendaAndContent from "../components/workshop/AgendaAndContent"
+import { graphql } from "gatsby"
+import WorkshopPeople from "../components/workshop/WorkshopPeople"
+
+export default function Workshop({ data }) {
+  return (
+    <Layout>
+      <SEO
+        image={data.ogImage.childImageSharp.fluid}
+        title="USACO for Absolute Beginners Workshop"
+        description="CPI will be holding a USACO for Absolute Beginners Workshop on August 6th, 4-5pm PDT! Join us to learn more about what USACO is and how to get better at USACO!"
+      />
+
+      <Header noBanner />
+      <div className="margin-top-nav" />
+      {/*/!* Banner margin *!/*/}
+      {/*<div className="h-20 sm:h-12"/>*/}
+      <Hero />
+      <AgendaAndContent />
+      <WorkshopPeople />
+
+    </Layout>
+  )
+}
+export const pageQuery = graphql`
+  query {
+    ogImage: file(relativePath: { eq: "workshop.jpg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 1200) {
+          ...GatsbyImageSharpFluid_noBase64
+        }
+      }
+    }
+  }
+`
