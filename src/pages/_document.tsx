fec28662cdb5c8b9c132d23da3d38ba363c76950
maggie-j-liu <maggie.j.liu@gmail.com>
2021-09-11 16:14:31 -0700

    add inter font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+        </Head>
         <body>
           <Main />
           <NextScript />
