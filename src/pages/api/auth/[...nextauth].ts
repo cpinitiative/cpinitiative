d7a27987e6782ace542f6bc6f53784891a212638
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-09-12 14:47:31 -0500

    .

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,13 @@
+import NextAuth from "next-auth"
+import Providers from "next-auth/providers"
+
+
+export default NextAuth({
+  // Configure one or more authentication providers
+  providers: [
+    Providers.Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+})
