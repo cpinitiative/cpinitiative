bf89f6923d7559779b45fbd8aa55aed75a14f871
cskitty <melody.yue.yu@gmail.com>
2021-09-12 16:14:30 -0700

    Removed zoom
    1) removed zoom, sign up, contact us
    2) replaced with video

--- src/components/workshop/Hero.tsx
+++ src/components/workshop/Hero.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 // @ts-ignore
 import HeroImg from "../../images/ContestsHero"
 import EmailForm from "../classes/EmailForm"
+import Youtube from './markdown/Youtube';
 
 export default function Hero() {
   return (
@@ -18,39 +19,10 @@ export default function Hero() {
                   USACO for Absolute Beginners
                 </span>
               </h2>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
-                CPI will be holding a workshop "USACO for absolute Beginners" on
-                August 6th, 4pm PDT! Join us to learn more about what USACO is!
-              </p>
-              <p className="mt-1 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
-                Zoom link:{" "}
-                <a
-                  target="_blank"
-                  href="https://bit.ly/36ZwIKE"
-                  className="underline text-blue-600"
-                >
-                  https://bit.ly/36ZwIKE
-                </a>
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <a
-                    href="https://forms.gle/2A4wGF9if3f6uNoX7"
-                    target="_blank"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                  >
-                    Sign Up
-                  </a>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="mailto:ocmelodyyu@gmail.com"
-                    target="_blank"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-purple-700 bg-purple-100 hover:text-purple-600 hover:bg-purple-50 focus:outline-none focus:shadow-outline-purple focus:border-purple-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                  >
-                    Contact Us
-                  </a>
-                </div>
+  
+              <div className="relative block w-full min-h-32 lg:min-h-64 xl:min-h-80 rounded-lg overflow-hidden focus:outline-none focus:shadow-outline bg-gray-300">
+            <Youtube id={'o3zKQgPpNK0'} autoplay />
+         
               </div>
             </div>
           </main>
