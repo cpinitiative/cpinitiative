b9f3df0cc8722930f95de85c13be28daff0cd7d6
Jeffrey Meng <jeffreymeng@users.noreply.github.com>
2021-09-19 20:10:31 -0700

    Allow jay to view registrations

--- src/pages/classes/admin/view-registrations.tsx
+++ src/pages/classes/admin/view-registrations.tsx
@@ -108,7 +108,8 @@ export default function ViewRegistrationPage() {
             "OjLKRTTzNyQgMifAExQKUA4MtfF2",
             "v8NK8mHCZnbPQKaPnEs5lKNc3rv2",
             "BKFOe33Ym7Pc7aQuET57MiljpF03",
-            "YF9ObmH1SUR1MKJGTrO8DfBQUG13"
+            "YF9ObmH1SUR1MKJGTrO8DfBQUG13",
+            "5IXfZDX1j2ZOftqfYiBcmmStmn93"
           ].includes(user.uid)
         ) {
           setHasPermission(true)
