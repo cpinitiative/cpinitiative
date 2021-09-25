67dc9dd1cd2b1e9ac14051c27a385bcf7417c2a3
maggie-j-liu <maggie.j.liu@gmail.com>
2021-09-24 21:09:58 -0700

    add video to workshop page

--- src/components/workshop/Hero.tsx
+++ src/components/workshop/Hero.tsx
@@ -7,9 +7,9 @@ export default function Hero() {
   return (
     <div className="relative bg-white overflow-hidden">
       <div className="max-w-screen-xl mx-auto">
-        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <main className="pt-10 mx-auto max-w-screen-xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-18 lg:px-8 xl:pt-24">
-            <div className="sm:text-center lg:text-left">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-7xl lg:w-full mx-auto lg:pb-28 xl:pb-32">
+          <main className="pt-10 mx-auto max-w-screen-xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-18 lg:px-8 xl:pt-24 grid grid-cols-1 md:grid-cols-5 items-center">
+            <div className="sm:text-center lg:text-left md:col-span-2">
               <div className="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
                 August 6, 2021
               </div>
@@ -19,46 +19,23 @@ export default function Hero() {
                 </span>
               </h2>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
-                CPI will be holding a workshop "USACO for absolute Beginners" on
-                August 6th, 4pm PDT! Join us to learn more about what USACO is!
+                CPI held a "USACO for absolute Beginners" workshop on August
+                6th, 4pm PDT!
               </p>
-              <p className="mt-1 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
-                Zoom link:{" "}
-                <a
-                  target="_blank"
-                  href="https://bit.ly/36ZwIKE"
-                  className="underline text-blue-600"
-                >
-                  https://bit.ly/36ZwIKE
-                </a>
-              </p>
-              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                <div className="rounded-md shadow">
-                  <a
-                    href="https://forms.gle/2A4wGF9if3f6uNoX7"
-                    target="_blank"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                  >
-                    Sign Up
-                  </a>
-                </div>
-                <div className="mt-3 sm:mt-0 sm:ml-3">
-                  <a
-                    href="mailto:ocmelodyyu@gmail.com"
-                    target="_blank"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-purple-700 bg-purple-100 hover:text-purple-600 hover:bg-purple-50 focus:outline-none focus:shadow-outline-purple focus:border-purple-300 transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
-                  >
-                    Contact Us
-                  </a>
-                </div>
+            </div>
+            <div className={"md:col-span-3"}>
+              <div className="w-full aspect-w-16 aspect-h-9 rounded-lg overflow-hidden">
+                <iframe
+                  src="https://www.youtube.com/embed/o3zKQgPpNK0"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="w-full h-full"
+                />
               </div>
             </div>
           </main>
         </div>
-      </div>
-      <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-        {/*<Img className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" fluid={heroImg} alt="" />*/}
-        <HeroImg className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full lg:max-w-3xl lg:mx-auto" />
       </div>
     </div>
   )
