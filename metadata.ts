f9dbdd5ecda7176ae09c6ed364541b826a132e4e
Neo Wang <43394822+nwatx@users.noreply.github.com>
2021-09-25 19:42:47 -0500

    update metadata for emails

--- metadata.ts
+++ metadata.ts
@@ -1,34 +1,47 @@
 export const VolunteerInfo = {
   Aadit: {
     aliases: ["Aadit Ambadkar"],
+    emails: ["aadit.ambadkar@gmail.com"],
+  },
+  Ananth: {
+    aliases: ["Ananth Kashyap"],
+    emails: ["ananthkashyap4@gmail.com"],
+  },
+  Andrew: {
+    aliases: ["Andrew Cheng"],
+    emails: ["AndrewCheng827827@gmail.com"],
+  },
+  Arpan: {
+    aliases: ["Arpan Banerjee"],
+    emails: ["arnbnrch@gmail.com"],
+  },
+  Brad: {
+    aliases: ["Brad Ma"],
+    emails: ["bradma123456@gmail.com"],
+  },
+  Dong: {
+    aliases: ["Dong Liu"],
+    emails: ["dongliu0426@gmail.com"],
+  },
+  Dustin: {
+    aliases: ["Dustin Miao"],
+    emails: ["dutin20.meow@gmail.com"],
   },
   Harry: {
     aliases: ["Qi Wang"],
     emails: ["harrynicomama@gmail.com"],
   },
-  Ananth: {
-    aliases: ["Ananth Kashyap"],
-  },
-  Arpan: {
-    aliases: ["Arpan Banerjee"],
-  },
-  Brad: {
-    aliases: ["Brad Ma"],
-  },
-  Dong: {
-    aliases: ["Dong Liu"],
-  },
   Jesse: {
     aliases: ["Jesse Choe"],
-  },
-  Dustin: {
-    aliases: ["Dustin Miao"],
+    emails: ["jessechoe10@gmail.com"],
   },
   Kevin: {
     aliases: ["Kevin Sheng"],
+    emails: ["kevinsheng2005@gmail.com"],
   },
   Maggie: {
     aliases: ["Maggie Liu"],
+    emails: ["maggie.j.liu@gmail.com"],
   },
   Melody: {
     aliases: ["Melody Yu"],
@@ -36,27 +49,30 @@ export const VolunteerInfo = {
   },
   Mrinall: {
     aliases: ["Mrinall Umasudhan"],
+    emails: ["bossguy124567@gmail.com"],
   },
   "Nathan Gong": {
-    aliases: ["Melody Yu"],
+    aliases: ["Nathan Gong"],
+    emails: ["nathan.gong22@bcp.org", "nathangong1234@gmail.com"],
   },
   "Neo Wang": {
-    emails: ["neowangatx@gmail.com", "wanggdash@gmail.com"],
     aliases: ["Neo Wang"],
+    emails: ["neowangatx@gmail.com", "wanggdash@gmail.com"],
   },
   Oscar: {
     aliases: ["Oscar", "Oscar Garries", "OscarGarrius"],
+    emails: ["oscargarries@gmail.com"],
   },
   Ryan: {
     aliases: ["Ryan Chou"],
+    emails: ["ryanchou111@gmail.com"],
   },
   Sathvik: {
     aliases: ["Sathvik Chundru"],
+    emails: ["24sathvikc@students.harker.org"],
   },
   "Sofia Yang": {
     aliases: ["Sofia Yang"],
-  },
-  "Ben Dodge": {
-    aliases: ["Ben Dodge"],
+    emails: ["altriatree@gmail.com"],
   },
 }
