f1c935b959118e9a8c3d9176c994d2045884461a
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-04 15:51:19 -0700

    refactor

--- src/components/workshop/WorkshopPeople.tsx
+++ src/components/workshop/WorkshopPeople.tsx
@@ -1,14 +1,6 @@
-import * as React from "react"
-import Image from "next/image"
 import { nathanw, melody } from "../index/images"
-
-type Member = {
-  photo: StaticImageData // url of photo relative to content/authors/images/, EXCLUDING EXTENSION
-  name: string
-  title?: string
-}
-
-const members: Member[] = [
+import Speakers, { Speaker } from "../workshops/Speakers"
+const speakers: Speaker[] = [
   {
     photo: nathanw,
     name: "Nathan Wang",
@@ -20,47 +12,6 @@ const members: Member[] = [
     title: "Instructor",
   },
 ]
-
 export default function WebinarPeople() {
-  return (
-    <div className="bg-white">
-      <div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
-        <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
-          <div className="space-y-5 sm:space-y-4">
-            <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">
-              Meet the speakers
-            </h2>
-          </div>
-          <div className="lg:col-span-2">
-            <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
-              {members.map(member => (
-                <li key={member.name}>
-                  <div className="flex items-center space-x-4 lg:space-x-6">
-                    <div className="flex-shrink-0 w-16 h-16 rounded-full overflow-hidden relative lg:w-20 lg:h-20">
-                      <Image
-                        layout="fill"
-                        objectFit="cover"
-                        objectPosition="center center"
-                        src={member.photo}
-                        alt={member.name}
-                        placeholder="blur"
-                      />
-                    </div>
-                    <div className="font-medium text-lg leading-6 space-y-1">
-                      <h3>{member.name}</h3>
-                      {member.title && (
-                        <p className="text-indigo-600 text-base">
-                          {member.title}
-                        </p>
-                      )}
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+  return <Speakers speakers={speakers} />
 }
---
+++ src/components/workshops/Speakers.tsx
@@ -0,0 +1,61 @@
+import * as React from "react"
+import Image from "next/image"
+
+export type Speaker = {
+  photo: StaticImageData
+  name: string
+  title?: string
+}
+
+export default function Speakers({
+  speakers,
+  description,
+}: {
+  speakers: Speaker[]
+  description?: string
+}) {
+  return (
+    <div className="bg-white">
+      <div className="mx-auto py-12 px-4 max-w-screen-xl sm:px-6 lg:px-8 lg:py-24">
+        <div className="grid grid-cols-1 gap-12 lg:grid-cols-3 lg:gap-8">
+          <div className="space-y-5 sm:space-y-4">
+            <h2 className="text-3xl leading-9 font-extrabold tracking-tight sm:text-4xl">
+              Meet the speakers
+            </h2>
+            {description && (
+              <p className="text-xl leading-7 text-gray-500">{description}</p>
+            )}
+          </div>
+          <div className="lg:col-span-2">
+            <ul className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-12 sm:space-y-0 lg:gap-x-8">
+              {speakers.map(speaker => (
+                <li key={speaker.name}>
+                  <div className="flex items-center space-x-4 lg:space-x-6">
+                    <div className="flex-shrink-0 w-16 h-16 rounded-full overflow-hidden relative lg:w-20 lg:h-20">
+                      <Image
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="center center"
+                        src={speaker.photo}
+                        alt={speaker.name}
+                        placeholder="blur"
+                      />
+                    </div>
+                    <div className="font-medium text-lg leading-6 space-y-1">
+                      <h3>{speaker.name}</h3>
+                      {speaker.title && (
+                        <p className="text-indigo-600 text-base">
+                          {speaker.title}
+                        </p>
+                      )}
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
