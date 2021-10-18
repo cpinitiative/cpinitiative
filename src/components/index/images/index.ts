589998e8b6cfc1ac84baace073c11986b45249c8
maggie-j-liu <maggie.j.liu@gmail.com>
2021-10-17 17:41:46 -0700

    add more people

--- src/components/index/images/index.ts
+++ src/components/index/images/index.ts
@@ -1,5 +1,6 @@
 import aadit from "./aadit.jpg"
 import adham from "./adham.jpg"
+import akshar from "./akshar.png"
 import albert from "./albert.jpg"
 import albertz from "./albertz.jpg"
 import alex_chen from "./alex_chen.png"
@@ -21,6 +22,7 @@ import david_li from "./david_li.png"
 import davidz from "./davidz.jpg"
 import dong from "./dong.jpg"
 import dustin from "./dustin.jpg"
+import egor from "./egor.jpg"
 import harry from "./harry.jpg"
 import jarvis from "./jarvis.png"
 import jay from "./jay.jpg"
@@ -40,6 +42,7 @@ import neo from "./neo.jpg"
 import nikhil from "./nikhil.png"
 import oscar from "./oscar.png"
 import pranav from "./pranav.png"
+import ramit from "./ramit.jpg"
 import rayna from "./rayna.jpeg"
 import ryan from "./ryan.jpg"
 import sathvik from "./sathvik.png"
@@ -52,59 +55,4 @@ import vivian from "./vivian.jpg"
 import yifan_ma from "./yifan_ma.png"
 import yu_lim from "./yu_lim.png"
 import zhixin from "./zhixin.png"
-export {
-  aadit,
-  adham,
-  albert,
-  albertz,
-  alex_chen,
-  amogha_pokkulandra,
-  amy_chang,
-  ananth,
-  andrew,
-  arpan,
-  atharv,
-  ben,
-  benq,
-  blank,
-  brad,
-  claire_zhang,
-  cow,
-  daniel,
-  darren,
-  david_li,
-  davidz,
-  dong,
-  dustin,
-  harry,
-  jarvis,
-  jay,
-  jeffrey,
-  jesse,
-  jpaulson,
-  kevin,
-  maggie,
-  mayank,
-  melody,
-  michael,
-  mrinall,
-  nathanc,
-  nathang,
-  nathanw,
-  neo,
-  nikhil,
-  oscar,
-  pranav,
-  rayna,
-  ryan,
-  sathvik,
-  sofia,
-  tarushii,
-  timothy,
-  varun,
-  vikas_thoutam,
-  vivian,
-  yifan_ma,
-  yu_lim,
-  zhixin,
-}
+export { aadit, adham, akshar, albert, albertz, alex_chen, amogha_pokkulandra, amy_chang, ananth, andrew, arpan, atharv, ben, benq, blank, brad, claire_zhang, cow, daniel, darren, david_li, davidz, dong, dustin, egor, harry, jarvis, jay, jeffrey, jesse, jpaulson, kevin, maggie, mayank, melody, michael, mrinall, nathanc, nathang, nathanw, neo, nikhil, oscar, pranav, ramit, rayna, ryan, sathvik, sofia, tarushii, timothy, varun, vikas_thoutam, vivian, yifan_ma, yu_lim, zhixin }
