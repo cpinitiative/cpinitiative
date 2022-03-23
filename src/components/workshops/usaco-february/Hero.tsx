9ab14b7a0de2c0fbe890e738bf3b7a2c990937ee
maggie-j-liu <maggie.j.liu@gmail.com>
2022-03-22 23:05:57 -0700

    add feb workshop videos

--- src/components/workshops/usaco-february/Hero.tsx
+++ src/components/workshops/usaco-february/Hero.tsx
@@ -1,13 +1,12 @@
 import * as React from "react"
-import Logo from "../../../images/x-camp_cpi.png"
-import Image from "next/image"
+import Youtube from "../../Youtube"
 export default function Hero() {
   return (
     <div className="relative bg-white overflow-hidden">
       <div className="max-w-screen-xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-7xl lg:w-full mx-auto lg:pb-28 xl:pb-32">
-          <main className="pt-10 mx-auto max-w-screen-xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-18 lg:px-8 xl:pt-24 grid grid-cols-1 lg:grid-cols-2 items-start gap-8">
-            <div className="sm:text-center lg:text-left">
+          <main className="pt-10 mx-auto max-w-screen-xl px-4 sm:pt-12 sm:px-6 md:pt-16 lg:pt-18 lg:px-8 xl:pt-24 grid grid-cols-1 lg:grid-cols-5 items-start gap-8">
+            <div className="sm:text-center lg:text-left lg:col-span-2">
               <div className="text-sm font-semibold uppercase tracking-wide text-gray-500 sm:text-base lg:text-sm xl:text-base">
                 March 6, 2022
               </div>
@@ -24,33 +23,14 @@ export default function Hero() {
                 covered from <b>10am-12pm PST</b>, and the Gold division will be
                 discussed from <b>4pm-5pm PST</b>.
               </p>
-              <p className="mt-1 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:text-xl lg:mx-0">
-                Bronze/Silver Session Zoom:{" "}
-                <a
-                  target="_blank"
-                  href="https://us06web.zoom.us/j/83196328567"
-                  className="underline text-blue-600"
-                >
-                  ID: 831 9632 8567
-                </a>
-                <br />
-                Gold Session Zoom:{" "}
-                <a
-                  target="_blank"
-                  href="https://us06web.zoom.us/j/86739411537"
-                  className="underline text-blue-600"
-                >
-                  ID: 867 3941 1537
-                </a>
-              </p>
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <a
-                    href="https://forms.gle/QUiNote2bzFXjGHQ8"
+                    href="https://forms.gle/R3d3gvEGqsEbMZ8cA"
                     target="_blank"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-purple-600 hover:bg-purple-500 focus:outline-none focus:border-purple-700 focus:shadow-outline-purple transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10"
                   >
-                    Sign Up
+                    Join Mailing List
                   </a>
                 </div>
                 <div className="mt-3 sm:mt-0 sm:ml-3">
@@ -64,14 +44,9 @@ export default function Hero() {
                 </div>
               </div>
             </div>
-            <div className={"w-full max-w-2xl mx-auto"}>
-              <Image
-                src={Logo}
-                placeholder="blur"
-                priority
-                layout="responsive"
-                className="rounded-lg"
-              />
+            <div className={"lg:col-span-3 w-full max-w-2xl mx-auto space-y-6"}>
+              <Youtube id="MhghUbs77ZQ" />
+              <Youtube id="mVmfw9fHOKA" />
             </div>
           </main>
         </div>
