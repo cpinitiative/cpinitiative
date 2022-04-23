a730adc0364df9ab40a5b5921ec8795d14aa7c26
Trua Shamu <sofia_mit@hotmail.com>
2022-04-23 14:11:53 -0500

    remove 'feature: office hours' from classes

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -102,51 +102,7 @@ export default function Classes() {
                   </span>
                 </p>
 
-                {/* Feature: Office Hours */}
-                <div className="bg-white px-4">
-                  <div className="relative py-8">
-                    <div className="sm:mx-auto max-w-md sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-                      <div className="relative rounded-2xl px-8 py-8 bg-teal-700 overflow-hidden shadow-xl sm:px-12 sm:py-12 md:py-20">
-                        <div
-                          aria-hidden="true"
-                          className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0"
-                        >
-                          <svg
-                            className="absolute inset-0 h-full w-full"
-                            preserveAspectRatio="xMidYMid slice"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 1463 360"
-                          >
-                            <path
-                              className="text-teal-600 text-opacity-40"
-                              fill="currentColor"
-                              d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
-                            />
-                            <path
-                              className="text-teal-800 text-opacity-40"
-                              fill="currentColor"
-                              d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
-                            />
-                          </svg>
-                        </div>
-                        <div className="relative">
-                          <div className="text-left sm:text-center">
-                            <h2 className="text-2xl font-extrabold text-white tracking-tight sm:text-4xl">
-                              New Addition: Office Hours.
-                            </h2>
-                            <p className="mt-2 sm:mt-6 mx-auto max-w-2xl sm:text-lg text-teal-200">
-                              In order to ensure student success, we host office
-                              hours every Wednesday at 5pm PDT where students
-                              can ask for help from instructors.
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
+                
                 <div className="h-4" />
 
                 <div>
