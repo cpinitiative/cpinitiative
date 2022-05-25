3e9e2fff9f0b9b115de668be95ffdcfc60783eab
Stanley <101726102+zhao-stanley@users.noreply.github.com>
2022-05-24 21:36:30 -0400

    Update src/components/contests/ContestCard.tsx
    Remove truncate for contest names.
    
    Co-authored-by: Maggie Liu <63619830+maggie-j-liu@users.noreply.github.com>

--- src/components/contests/ContestCard.tsx
+++ src/components/contests/ContestCard.tsx
@@ -24,7 +24,7 @@ export default function ContestCard({ name, date, photo, link, isPast, isToday }
           </div> : null}
 
           <div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-black px-6 pb-6 md:px-4 md:pb-4 lg:px-3 lg:pb-3 pt-12">
-            <p className="mt-2 block md:text-xl text-2xl font-bold text-white truncate pointer-events-none">
+            <p className="mt-2 block md:text-xl text-2xl font-bold text-white pointer-events-none">
               {name}
             </p>
             <p className="block md:text-base sm:text-lg font-medium text-gray-200 pointer-events-none">
