de72c481853c5a114c5c0293e21886eeb56748d5
Nathan Wang <nathan.r.wang@gmail.com>
2022-10-28 17:58:34 -0400

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,17 @@ import { FirebaseProvider } from "../firebase/FirebaseContext"
 import { SessionProvider } from "next-auth/react"
 import "../styles/main.css"
 import "../styles/tailwindcss-utils.css"
+import { Analytics } from '@vercel/analytics/react';
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
     <SessionProvider session={pageProps.session}>
       <FirebaseProvider>
         <Component {...pageProps} />
       </FirebaseProvider>
     </SessionProvider>
+    <Analytics />
+    </>
   )
 }
