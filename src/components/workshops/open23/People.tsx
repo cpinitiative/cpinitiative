08f0a18580b704af0bb88db3d0ca6b96352df24f
mjh316 <justinhuang316@gmail.com>
2023-04-01 23:20:58 -0700

    Fixed presenters on workshop page

--- src/components/workshops/open23/People.tsx
+++ src/components/workshops/open23/People.tsx
@@ -5,12 +5,12 @@ import {
   amogha,
   aditya,
   ryan_fu,
+  kevin,
 } from "../../index/images"
 import xieting from "../../../images/xieting.jpeg"
 import rohan_garg from "../../index/images/rohan_garg.jpg"
 import chongtian_ma from "../../index/images/chongtian_ma.jpg"
 import albert_ye from "../../index/images/albert_ye.jpeg"
-import { nikhil } from "../../index/images"
 
 import Speakers, { Speaker } from "../Speakers"
 const speakers: Speaker[] = [
@@ -25,11 +25,6 @@ const speakers: Speaker[] = [
     title: "Teacher / CPI",
   },
   {
-    photo: aditya,
-    name: "Aditya Gupta",
-    title: "Teacher / CPI",
-  },
-  {
     photo: ryan,
     name: "Ryan Chou",
     title: "Teacher / CPI",
@@ -40,8 +35,8 @@ const speakers: Speaker[] = [
     title: "Teacher / CPI",
   },
   {
-    photo: nikhil,
-    name: "Nikhil Chatterjee",
+    photo: kevin,
+    name: "Kevin Sheng",
     title: "Teacher / CPI",
   },
 ]
