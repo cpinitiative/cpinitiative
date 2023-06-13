9cd9426d1c0107ef12902eb0e60e63a089c6a8ed
Ady <advayb2018@gmail.com>
2023-06-12 22:05:30 -0700

    reformat contests

--- src/pages/contests.tsx
+++ src/pages/contests.tsx
@@ -9,6 +9,17 @@ import ContestList from "../components/contests/ContestList"
 import PartneredContests from "../components/contests/PartneredContests"
 import CurrentClubs from "../components/clubs/CurrentClubs"
 import Link from "next/link"
+import Image from "next/image"
+import {
+  taco, 
+  lit,
+  algirlithm,
+  proco1,
+  proco2
+} from "../components/contests/images"
+import classNames from 'classnames';
+
+
 
 export default function Contests() {
   return (
@@ -68,12 +79,213 @@ export default function Contests() {
         </div>
         <div className="prose prose-lg text-gray-500 mx-auto mb-6">
           <p className = "text-1xl text-center">
-          Join high-quality programming contests hosted by students, for students.
+          Join high-quality programming contests hosted by students, for students. 
+          These contests are partnered and part of the CPI Competition Program.
           </p>
         </div>
-      
+        <div>
+          <h2 className="pt-1 mt-4 mb-8 text-4xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+              Partnered Tournaments
+          </h2>
+            <div>
+              <h3 className="text-xl sm:text-2xl font-semibold tracking-tight text-purple-600 mb-2 p-2 px-7">
+                Lexington Informatics Tournament (LIT)
+              </h3>
+              {/* Write code to display Image and a paragraph to the right of the image with text. */}
+              <div className="flex flex-row">
+                <div className="flex flex-col p-2 px-6">
+                  <Image
+                    objectPosition="center center"
+                    width={485}
+                    height={300}
+                    src={lit}
+                  />
+                  </div>
+                    <div className="flex flex-col prose prose-lg text-gray-500 mx-auto m-2 px-4">
+                      <p className = "text-1xl text-center">
+                      LIT (Lexington Informatics Tournament) is a competitive programming tournament for 
+                      middle/high school students, hosted by members of the LexMACS club from Lexington High School, 
+                      along with many guest problemsetters/testers. 
+                      </p>
 
-      <div className="relative bg-purple-600">
+                      <a
+                        href="https://lit.lhsmathcs.org/"
+                        target="_blank"
+                        className="flex items-center justify-center px-4 py-3 m-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-purple-600 bg-opacity-90 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Find Out More
+                      </a>
+                      <a
+                        href="https://discord.gg/k6yzFdZ"
+                        target="_blank"
+                        className="flex items-center justify-center px-4 py-3 m-2 border text-base font-medium rounded-md shadow-sm text-white bg-purple-600 bg-opacity-90 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Join their Discord!
+                      </a>
+
+                    </div>
+                </div>
+            </div>
+            <div>
+              <h3 className="text-xl sm:text-2xl font-semibold tracking-tight text-purple-600 mb-2 p-2 px-7">
+              The Andover Computing Open (TACO)
+              </h3>
+              {/* Write code to display Image and a paragraph to the right of the image with text. */}
+              <div className="flex flex-row">
+                <div className="flex flex-col p-2 px-6">
+                  <Image
+                    objectPosition="center center"
+                    width={440}
+                    height={300}
+                    src={taco}
+                  />
+                  </div>
+                    <div className="flex flex-col prose prose-lg text-gray-500 mx-auto m-2 px-4">
+                      <p className = "text-1xl text-center">
+                      The Andover Computing Open (TACO) is a secondary school tournament 
+                      run and designed by Phillips Academy Andover students. Compete for prizes, attend workshops, 
+                      and join a community of programmers!
+                      </p>
+                      <a
+                        href="https://www.andovercomputingopen.com/"
+                        target="_blank"
+                        className="flex items-center justify-center px-4 py-3 m-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-purple-600 bg-opacity-90 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Find Out More
+                      </a>
+                      <a
+                        href="https://discord.com/invite/s5Wp2QaSCJ"
+                        target="_blank"
+                        className="flex items-center justify-center px-4 py-3 m-2 border text-base font-medium rounded-md shadow-sm text-white bg-purple-600 bg-opacity-90 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Join their Discord!
+                      </a>
+                      
+                    </div>
+                </div>
+            </div>
+            <div>
+              <h3 className="text-xl sm:text-2xl font-semibold tracking-tight text-purple-600 mb-2 p-2 px-7">
+              alGIRLithm
+              </h3>
+              {/* Write code to display Image and a paragraph to the right of the image with text. */}
+              <div className="flex flex-row">
+                <div className="flex flex-col p-2 px-6">
+                  <Image
+                    objectPosition="center center"
+                    width={450}
+                    height={300}
+                    src={algirlithm}
+                  />
+                  </div>
+                    <div className="flex flex-col prose prose-lg text-gray-500 mx-auto m-2 px-4">
+                      <p className ="text-1xl text-center">
+                      alGIRLithm is a student-led high school programming contest working to inspire the next generation 
+                      of female computer scientists by making competitive programming accessible to high school girls.
+                      Girls with all levels of experience in competitive programming are encouraged to join!
+                      </p>
+                      
+                      <a
+                        href="https://algirlithm.org/"
+                        target="_blank"
+                        className="flex items-center justify-center px-4 py-3 m-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-purple-600 bg-opacity-90 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Find Out More
+                      </a>
+                      <a
+                        href="https://discord.gg/bqRBv3CfQr"
+                        target="_blank"
+                        className="flex items-center justify-center px-4 py-3 m-2 border text-base font-medium rounded-md shadow-sm text-white bg-purple-600 bg-opacity-90 hover:bg-opacity-70 sm:px-8"
+                      >
+                        Join their Discord!
+                      </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="text-lg max-w-prose mx-auto mb-6 pt-16">
+          <h1 className="mt-2 mb-8 text-4xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+            Unique Contest Hosting Platform
+          </h1>
+        </div>
+
+        <div className="prose prose-lg text-gray-500 mx-auto mb-6">
+          <p className = "text-1xl text-center">
+          We are proud to announce that we are partnered with ProCo, a unique contest hosting platform. ProCo 
+          allows students to host their own contests and events easily.
+          </p>
+        </div>
+
+        <div className="relative">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+            <div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  Free of Charge
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                ProCo allows users to host and run their own contests on their platform at no cost.
+                Nothing on the platform is locked behind a paywall. 
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0">
+            <a
+              className="block px-4 sm:px-6 lg:px-0 lg:m-0 lg:relative lg:h-full"
+            >
+
+              <Image
+                  className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
+                  src={proco1}
+                />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
+          <div className="px-4 max-w-xl mx-auto sm:px-6 lg:py-32 lg:max-w-none lg:mx-0 lg:px-0 lg:col-start-2">
+            <div>
+              <div>
+              </div>
+              <div className="mt-6">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900">
+                  Easy to Use
+                </h2>
+                <p className="mt-4 text-lg text-gray-500">
+                ProCo offers easy problem submissions and clean UI to expedite
+                the process of hosting a contest.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
+            <div className="block px-4 sm:px-6 lg:px-0 lg:m-0 lg:relative lg:h-full">
+              <Image
+                className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
+                src={proco2}
+                />
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <div className="prose prose-lg text-gray-500 mx-auto mb-6">
+          <p className = "text-1xl text-center">
+          <a
+            href="https://proco.vercel.app/"
+            target="_blank"
+            className="flex items-center justify-center px-4 py-3 m-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white sm:px-8 bg-gradient-to-r from-purple-600 to-indigo-600 hover:from-purple-700 hover:to-indigo-700"
+          >
+          Join Today!
+          </a>
+          </p>
+        </div>
+        
+      <div className="relative bg-purple-600 my-4">
         <div className="absolute inset-0">
           <div
             className="absolute inset-0 bg-purple-800 mix-blend-multiply"
@@ -89,47 +301,7 @@ export default function Contests() {
           </p>
         </div>
       </div>  
-        <ContestList />
-
-      <div className="relative bg-purple-600">
-        <div className="absolute inset-0">
-          <div
-            className="absolute inset-0 bg-purple-800 mix-blend-multiply"
-            aria-hidden="true"
-          />
-        </div>
-        <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-          <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
-            Partnered Contests
-          </h1>
-          <p className="mt-6 text-xl text-purple-100 max-w-3xl">
-            These contests are directly affiliated with the CP Initiative.
-          </p>
-        </div>
-      </div>  
-        <PartneredContests />
-
-        <div className="text-lg max-w-prose mx-auto mb-6">
-          <h1 className="mt-2 mb-8 text-4xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-            Unique Contest Hosting Platform
-          </h1>
-        </div>
-        
-        <div className="prose prose-lg text-gray-500 mx-auto mb-6">
-        <img 
-            width = "950px"
-            src="https://cdn.discordapp.com/attachments/877330703853961268/1110262012161831056/Screen_Shot_2023-05-03_at_9.png"
-            placeholder="blur"
-          />
-
-          <p className = "text-1xl text-center">
-            Host your own contest on our partnered platform, ProCo. <br></br> 
-            <a href="https://proco.vercel.app/login" className="underline">
-                Join today!
-            </a>
-          </p>
-        </div>
-
+        <ContestList /> 
 
         <div className="relative pt-15 pb-15" id="how-to-join">
         <div className="max-w-prose sm:text-lg mx-auto px-4">
