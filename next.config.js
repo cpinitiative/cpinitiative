0fab7c976702e62f6062f1f14b9bd8c59d38e8b8
mjh <61671361+mjh316@users.noreply.github.com>
2024-02-03 20:51:05 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,43 +5,39 @@ const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  async redirects() {
+     return [
+       {
+         source: "/workshop",
+         destination: "/workshops",
+         permanent: true,
+       },
+       {
+         source: "/webinar",
+         destination: "/workshops/intro-to-usaco",
+         permanent: true,
+       },
+       {
+         source: "/workshop/beginner",
+         destination: "/workshops/beginner",
+         permanent: true,
+       },
+       {
+         source: "/workshop/egoi",
+         destination: "/workshops/egoi",
+         permanent: true,
+       },
+       {
+         source: "/workshop/intro-to-usaco",
+         destination: "/workshops/intro-to-usaco",
+         permanent: true,
+       },
+     ]
+   },
+   typescript: {
+     //
+     ignoreBuildErrors: true,
+   },
 }
  
 module.exports = withMDX(nextConfig)
-
-module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/workshop",
-        destination: "/workshops",
-        permanent: true,
-      },
-      {
-        source: "/webinar",
-        destination: "/workshops/intro-to-usaco",
-        permanent: true,
-      },
-      {
-        source: "/workshop/beginner",
-        destination: "/workshops/beginner",
-        permanent: true,
-      },
-      {
-        source: "/workshop/egoi",
-        destination: "/workshops/egoi",
-        permanent: true,
-      },
-      {
-        source: "/workshop/intro-to-usaco",
-        destination: "/workshops/intro-to-usaco",
-        permanent: true,
-      },
-    ]
-  },
-  typescript: {
-    //
-    ignoreBuildErrors: true,
-  },
-}
-
