658e867e428f9015efde56f56bae93fc319e887f
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2024-02-07 13:00:23 -0600

    Reverted from es modules

--- next.config.js
+++ next.config.js
@@ -1,43 +1,46 @@
-import withMDX = require('@next/mdx')()
+const withMDX = require('@next/mdx')()
  
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  async redirects() {
-     return [
-       {
-         source: "/workshop",
-         destination: "/workshops",
-         permanent: true,
-       },
-       {
-         source: "/webinar",
-         destination: "/workshops/intro-to-usaco",
-         permanent: true,
-       },
-       {
-         source: "/workshop/beginner",
-         destination: "/workshops/beginner",
-         permanent: true,
-       },
-       {
-         source: "/workshop/egoi",
-         destination: "/workshops/egoi",
-         permanent: true,
-       },
-       {
-         source: "/workshop/intro-to-usaco",
-         destination: "/workshops/intro-to-usaco",
-         permanent: true,
-       },
-     ]
-   },
-   typescript: {
-     //
-     ignoreBuildErrors: true,
-   },
 }
  
 module.exports = withMDX(nextConfig)
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/workshop",
+        destination: "/workshops",
+        permanent: true,
+      },
+      {
+        source: "/webinar",
+        destination: "/workshops/intro-to-usaco",
+        permanent: true,
+      },
+      {
+        source: "/workshop/beginner",
+        destination: "/workshops/beginner",
+        permanent: true,
+      },
+      {
+        source: "/workshop/egoi",
+        destination: "/workshops/egoi",
+        permanent: true,
+      },
+      {
+        source: "/workshop/intro-to-usaco",
+        destination: "/workshops/intro-to-usaco",
+        permanent: true,
+      },
+    ]
+  },
+  typescript: {
+    //
+    ignoreBuildErrors: true,
+  },
+}
