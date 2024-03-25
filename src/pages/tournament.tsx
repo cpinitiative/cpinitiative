f09cd819fbbff78bbef8bb918ff469bdc82097f5
Vinceyou1 <113566592+Vinceyou1@users.noreply.github.com>
2024-03-24 23:51:48 -0500

    Added results button
    also some other minor formatting stuff

--- src/pages/tournament.tsx
+++ src/pages/tournament.tsx
@@ -134,34 +134,37 @@ export default function Tournament() {
               <h2 className="text-lg text-gray-500 font-semibold md:text-xl lg:text-2xl pt-4 pb-2 py-4 mx-auto text-center">
                 Saturday, March 2nd, 2024 <br /> 10 AM - 1 PM PST
               </h2>
-              <div className="flex justify-around gap-x-4 w-fit mx-auto">
-                {/* <Link
-                  className="mx-auto text-white rounded-xl focus:translate-y-1 hover:shadow-inner hover:shadow-gray-800 transition-all duration-150 mt-4 bg-[#8976FF] lg:text-xl px-8 py-3"
-                  href={CONTEST_REGISTRATION_FORM_LINK}
-                >
-                  Register &rarr;
-                </Link> */}
-                <Link
-                  className="mx-auto text-white bg-[#8976ff]/40 rounded-xl focus:translate-y-1 hover:underline transition-all duration-150 mt-4 lg:text-xl px-8 py-2"
-                  href={"https://codeforces.com/contests/105009"}
-                >
-                  View problems &rarr;
-                </Link>
+              <div className="flex flex-col items-stretch  ml-auto mr-auto">
+                <div className="flex w-full justify-around gap-x-2 mx-auto">
+                  <Link
+                    className="text-center flex-grow mx-auto text-white bg-[#8976ff]/100 rounded-lg focus:translate-y-1 hover:underline transition-all duration-150 mt-4 lg:text-xl px-8 py-2"
+                    href="/tournament/2024-results"
+                  >
+                    Results &rarr;
+                  </Link>
+                  <Link
+                    className="mx-auto text-white bg-[#8976ff]/40 rounded-lg focus:translate-y-1 hover:underline transition-all duration-150 mt-4 lg:text-xl px-8 py-2"
+                    href={"https://codeforces.com/contests/105009"}
+                  >
+                    View problems &rarr;
+                  </Link>
+                </div>
+                <div className=" flex justify-around gap-x-2 w-fit mx-auto my-2">
+                  <Link
+                    href={"/" + CONTEST_EDITORIAL_FILE}
+                    className="bg-[#8976FF]/60 text-white z-10 w-fit mx-auto px-6 py-2 lg:text-xl text-lg rounded-lg"
+                  >
+                    View Editorial &rarr;
+                  </Link>
+                  <Link
+                    href={"/" + CONTEST_SOLUTIONS_FILE}
+                    className="bg-[#8976FF]/80 text-white z-10 w-fit mx-auto px-6 py-2 lg:text-xl text-lg rounded-lg"
+                  >
+                    Download Solutions &rarr;
+                  </Link>
+                </div>
               </div>
-              <div className="flex justify-around gap-x-4 w-fit mx-auto my-2">
-                <Link
-                  href={"/" + CONTEST_EDITORIAL_FILE}
-                  className="bg-[#8976FF]/80 text-white z-10 w-fit mx-auto px-6 py-2 lg:text-xl text-lg rounded-lg"
-                >
-                  View Editorial &rarr;
-                </Link>
-                <Link
-                  href={"/" + CONTEST_SOLUTIONS_FILE}
-                  className="bg-[#8976FF]/60 text-white z-10 w-fit mx-auto px-6 py-2 lg:text-xl text-lg rounded-lg"
-                >
-                  Download Solutions &rarr;
-                </Link>
-              </div>
+              
               <Link
                 className="font-medium underline underline-offset-4 hover:opacity-60 mx-auto pt-2 transition-opacity duration-150"
                 href={"https://discord.gg/SutDSVX6Zt"}
@@ -205,7 +208,7 @@ export default function Tournament() {
             <Image src={cpithink} alt="CPI!" className="md:-ml-0 -ml-4" />
           </div>
           <br></br>
-          <div className="lg:basis-1/3   font-medium mx-auto text-xl md:text-2xl ">
+          <div className="lg:basis-1/3 pb-8 lg:pb-0 font-medium mx-auto text-xl md:text-2xl ">
             The USACO.Guide Informatics Tournament is a programming contest for
             precollege students hosted by the{" "}
             <a
@@ -225,7 +228,7 @@ export default function Tournament() {
           <div className="lg:basis-1/3   font-medium mx-auto text-xl md:text-2xl text-center">
             The contest is over now, <br/> check out the results {" "}
             <a
-              href="https://usaco.guide"
+              href="/tournament/2024-results"
               target="_blank"
               rel="noreferrer"
               className="font-bold text-blue-700"
