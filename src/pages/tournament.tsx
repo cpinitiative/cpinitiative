77368a3af4d7ce7d1695a2fb632a6c96dc8ebd4c
ryanchou-dev <ryanchou111@gmail.com>
2025-03-03 21:55:42 -0800

    date hotfix

--- src/pages/tournament.tsx
+++ src/pages/tournament.tsx
@@ -119,7 +119,7 @@ export default function Tournament() {
               <br /> Informatics Tournament
             </h1>
             <h2 className="text-lg text-gray-500 font-semibold md:text-xl lg:text-2xl pt-4 pb-2 py-4 mx-auto text-center">
-              Saturday, March 2nd, 2025 <br /> 10:30 AM - 1:30 PM PDT
+              Saturday, March 15th, 2025 <br /> 10:30 AM - 1:30 PM PDT
             </h2>
             <div className="flex flex-col items-stretch  ml-auto mr-auto">
               <Link
