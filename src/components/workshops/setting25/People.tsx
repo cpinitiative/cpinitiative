4daddb17223fdee7f2300d5c78aa45679033c9fa
Ryan Chou <81596991+ryanchou-dev@users.noreply.github.com>
2025-04-01 22:41:51 -0700

    Update People.tsx

--- src/components/workshops/setting25/People.tsx
+++ src/components/workshops/setting25/People.tsx
@@ -2,6 +2,7 @@ import { benq } from "../../index/images"
 import chongtian_ma from "../../index/images/chongtian_ma.jpg"
 import aakash from "../../index/images/aakash.png"
 import ryan from "../../index/images/ryan_fu.jpeg"
+import suhas_nagar from '../../index/images/suhas_nagar.png'
 
 import Speakers, { Speaker } from "../Speakers"
 
