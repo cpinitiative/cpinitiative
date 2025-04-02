7949289bb707400a0917eb7d7741f6eb4743200d
RyanFu08 <108840013+RyanFu08@users.noreply.github.com>
2025-04-01 19:59:39 -0700

    Create setting25.tsx

---
+++ src/pages/workshops/setting25.tsx
@@ -0,0 +1,24 @@
+import * as React from "react"
+import Layout from "../../components/Layout"
+import SEO from "../../components/SEO"
+import Hero from "../../components/workshops/setting25/Hero"
+import Header from "../../components/Header"
+import People from "../../components/workshops/setting25/People"
+
+export default function USACOFebruary() {
+  return (
+    <Layout>
+      <SEO
+        image="../../images/workshops/setting25.png"
+        title="Problemsetting Panel Discussion"
+        description="Watch USACO Problemsetters discuss problem creation and answer your Q&A—live!"
+      />
+      <Header noBanner />
+      <div className="margin-top-nav" />
+      {/*/!* Banner margin *!/*/}
+      {/*<div className="h-20 sm:h-12"/>*/}
+      <Hero />
+      <People />
+    </Layout>
+  )
+}
