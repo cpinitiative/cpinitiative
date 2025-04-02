09a86d802db7b62fcd9af39c79bf0d5d520a211d
RyanFu08 <108840013+RyanFu08@users.noreply.github.com>
2025-04-01 19:57:19 -0700

    Create People.tsx

---
+++ src/components/workshops/setting25/People.tsx
@@ -0,0 +1,33 @@
+import { benq } from "../../index/images"
+import chongtian_ma from "../../index/images/chongtian_ma.jpg"
+import aakash from "../../index/images/aakash.png"
+import ryan from "../../index/images/ryan_fu.jpeg"
+
+import Speakers, { Speaker } from "../Speakers"
+
+const speakers: Speaker[] = [
+  {
+    name: "Ryan Fu",
+    title: "Host/CPI",
+    photo: ryan,
+  },
+  {
+    name: "Aakash Gokhale",
+    title: "USACO Problemsetter",
+    photo: aakash,
+  },
+  {
+    name: "Benjamin Qi",
+    title: "USACO Problemsetter",
+    photo: benq,
+  },
+  {
+    name: "Suhas Nagar",
+    title: "USACO Problemsetter",
+    photo: suhas_nagar,
+  }
+]
+
+export default function People() {
+  return <Speakers speakers={speakers} titleColor="text-blue-700" />
+}
