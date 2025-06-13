71c92580e0896b1bf86ec2bb3526f22b8b458e64
RyanFu08 <ryanfu2008@gmail.com>
2025-06-12 23:50:36 -0700

    Fix classes info
    maybe the real CPI web devs were the friends we made along the way

--- src/components/classes/registration/CourseSelectionSection.tsx
+++ src/components/classes/registration/CourseSelectionSection.tsx
@@ -63,8 +63,8 @@ export default function CourseSelectionSection({ level, setLevel }) {
 
                 <p className="mt-2 text-sm text-gray-500">
                   {level == "beginner"
-                    ? "Every Sunday (excluding USACO weekends) from February 9 - April 27 , 5-7pm PDT. (Total 10 Sessions)"
-                    : "Every Saturday (excluding USACO weekends) from February 8 - April 26, 4-6pm PDT. (Total 10 Sessions)"}
+                    ? "Every Sunday (excluding USACO weekends) from June 22 - August 24 , 5-7pm PDT. (Total 10 Sessions)"
+                    : "Every Saturday (excluding USACO weekends) from June 21 - August 23, 4-6pm PDT. (Total 10 Sessions)"}
                 </p>
               </div>
             )}
