f3ed90362a3a6c692cf04a666bc5d3045d1b1269
emhgit <eharper0815@gmail.com>
2025-09-01 10:31:41 -0500

    update dates in src\components\classes\registration\CourseSelectionSection.tsx to fall 2025 session

--- src/components/classes/registration/CourseSelectionSection.tsx
+++ src/components/classes/registration/CourseSelectionSection.tsx
@@ -63,10 +63,8 @@ export default function CourseSelectionSection({ level, setLevel }) {
 
                 <p className="mt-2 text-sm text-gray-500">
                   {level == "beginner"
-
-                    ? "Every Sunday (excluding USACO weekends) from June 22 - August 24 , 5-7pm PDT. (Total 10 Sessions)"
-                    : "Every Saturday (excluding USACO weekends) from June 21 - August 23, 4-6pm PDT. (Total 10 Sessions)"}
-
+                    ? "Every Sunday (excluding USACO weekends) from September 21 - November 23 , 5-7pm PDT. (Total 10 Sessions)"
+                    : "Every Saturday (excluding USACO weekends) from September 20 - November 22, 4-6pm PDT. (Total 10 Sessions)"}
                 </p>
               </div>
             )}
