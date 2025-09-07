5b4969994c757f4bfd6a4453d805ee7f60235aec
Ryan Chou <81596991+ryanchou-dev@users.noreply.github.com>
2025-09-07 12:46:38 -0700

    fix fin aid applications

--- src/components/classes/registration/FinancialAidSubmissionSection.tsx
+++ src/components/classes/registration/FinancialAidSubmissionSection.tsx
@@ -103,7 +103,7 @@ export default function FinancialAidSubmissionSection({
                   const ref = firebase
                     .firestore()
                     .collection("classes-registration")
-                    .doc("2025feb")
+                    .doc("2025oct")
                     .collection("registrations")
                     .doc()
                   setRegistrationId(ref.id)
